89170f57d7449d3718bd72566a275d65f7243e44
wesbosse <wes.bosse@gmail.com>
2020-10-20 08:53:40 -0700

    adding bronto route

--- server/server.js
+++ server/server.js
@@ -4,16 +4,28 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const fileUpload = require('express-fileupload');
 const axios = require('axios');
-const dotenv = require('dotenv').config();
+var fs = require('fs');
+var csvWriter = require('csv-write-stream')
 
-// const crypto = require('crypto');
-// const testData = require('../miva-post-raw.json');
+require('dotenv').config();
 
 // imported from mailer.js
 const mailer = require("./mailer")
 
 // create the express server
 const app = express();
+const imgMap = {
+    '10-1000': 'graphics/00000001/10-1000- 56 gallon capacity.jpg',
+    '100EE00AA2': 'graphics/00000001/EZ Lynk Auto Agent 2.0.jpg',
+    '10-1001': 'graphics/00000001/1001-V2.png',
+    '10-1002': 'graphics/00000001/10-1002 pictures.jpg',
+    '10-1004': 'graphics/00000001/10-1004 with single infographic.jpg',
+    '10-1005': 'graphics/00000001/MAIN10-1005- 14 Single Infographic.jpg',
+    '10-1006': 'graphics/00000001/10-1006- Single Infographic 7-19-19.jpg',
+    '10-1008': 'graphics/00000001/10-1008 thumbnail.jpg',
+    '10-2000': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
+    '10-2001': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
+};
 
 // middleware
 app.use(fileUpload({createParentPath: true}));
@@ -26,6 +38,10 @@ app.get('/',function(req,res){
     res.status(200).sendFile(path.join(__dirname + '/../testForm.html'));
 });
 
+app.get('/productImage/:sku', (req, res) => {
+    console.log(encodeURI(`http://sbtanks.com/mm5/${imgMap[req.params.sku]}`));
+});
+
 app.post('/register',function(req,res){
     mailer(req);
     res.redirect(301, 'https://www.sbfilters.com/new-dealer?submitted=true')
@@ -34,20 +50,6 @@ app.post('/register',function(req,res){
 app.post('/miva', async (req, res) => {
     console.log('in the miva route... ');
     const {id, bill_email, bill_fname, bill_lname, bill_addr1, bill_city, bill_state, bill_zip, bill_cntry, formatted_total, orderdate, items} = req.body;
-    // const {id, bill_email, bill_fname, bill_lname, bill_addr1, bill_city, bill_state, bill_zip, bill_cntry, formatted_total, orderdate, items} = testData;
-
-    const imgMap = {
-        '10-1000': 'graphics/00000001/10-1000- 56 gallon capacity.jpg',
-        '100EE00AA2': 'graphics/00000001/EZ Lynk Auto Agent 2.0.jpg',
-        '10-1001': 'graphics/00000001/1001-V2.png',
-        '10-1002': 'graphics/00000001/10-1002 pictures.jpg',
-        '10-1004': 'graphics/00000001/10-1004 with single infographic.jpg',
-        '10-1005': 'graphics/00000001/MAIN10-1005- 14 Single Infographic.jpg',
-        '10-1006': 'graphics/00000001/10-1006- Single Infographic 7-19-19.jpg',
-        '10-1008': 'graphics/00000001/10-1008 thumbnail.jpg',
-        '10-2000': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
-        '10-2001': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
-    };
     
     const formattedItems = items.map((item) => {
         const {sku, name, price} = item;
@@ -58,10 +60,9 @@ app.post('/miva', async (req, res) => {
             "productBrand": "SB Tanks",
             "productDescription": name,
             "productTitle": name,
-            "productImageUrl": encodeURI(`http://sbtanks.com/mm5/${imgMap[sku]}`),
+            "productImageUrl": encodeURI(`http://sbtanks.com/mm5/${imgMap[req.params.sku]}`),
             "productPrice": `$${price}`,
             "productType": "",
-            //this will need to be swapped for the live site
             "productUrl": `http://sbtanks.com/${nameCleanedforURL}`
         }
         return eachItemObj;
@@ -112,6 +113,43 @@ app.post('/miva', async (req, res) => {
     } catch (err) {
         console.log(`Error with stmaped post: ${err}`);
     }
+});
+
+app.post('/filtersBronto', async (req, res) => {
+    const {id, bill_email, bill_fname, bill_lname, bill_addr1, bill_city, bill_state, bill_zip, bill_cntry, formatted_total, orderdate, items} = req.body;
+    
+    // writing to the csv in append mode
+    var writer = csvWriter();
+    writer.pipe(fs.createWriteStream('bronto.csv', {flags: 'a'}));
+    
+    // bronto csv is on the item level. each order takes up multiple rows, etc.
+    const formattedItems = items.map((item) => {
+        const {sku, name, price, retail, base_price, total, quantity, sku} = item;
+        let nameCleanedforURL = name.replace(/[, ]+/g, "-").replace(/[.]+/g, "").trim().toLowerCase();
+
+        // updated to reflect robert's template
+        const eachItemObj = {
+            "email": bill_email,
+            "orderid": id,
+            "shipping_date": orderdate,
+            "shipping_details": "Shipped",
+            "productId": sku,
+            "name": name,
+            "quantity": quantity,
+            "unit_price": retail,
+            "sale_price": base_price,
+            "total_price": price,
+            "url": `http://sbtanks.com/${nameCleanedforURL}`,
+            "imageurl": "",
+        }
+
+        writer.write(eachItemObj);
+        return eachItemObj;
+    });
+
+    // kill the csv session
+    writer.end()
+
 });
 
 module.exports = app;
